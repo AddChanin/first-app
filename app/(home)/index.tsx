84027da294742ea4937ca06594bf0765252d47fb
AddChanin <102175316+AddChanin@users.noreply.github.com>
2025-11-01 10:40:35 +0700

    first commit

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -17,7 +17,7 @@ export default function Index() {
           params: {name: "Thomas"}
         }} 
         push asChild>
-        <Button>Parameter Buttton</Button>
+        <Button>New Parameter Buttton</Button>
       </Link>
 
       <Link href="/details/2" asChild>
